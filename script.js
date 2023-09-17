21490873e4ac4a0ad0a0f5165f5299083a2804c9
kelvin <kelvinhodeumavez@gmail.com>
2023-09-17 02:50:26 -0300

    Mínimo e máximo

--- script.js
+++ script.js
@@ -16,4 +16,23 @@ return false
 
 function arrayMaxMin(arr){
     /* Seu código aqui */
+let minimo = Infinity;
+let maximo = -Infinity;
+
+for (let i = 0; i < arr.length;i++) {
+if (arr[i] < minimo) {
+minimo = arr[i];
 }
+
+if (arr[i] > maximo){
+maximo = arr[i];
+}
+}
+return [minimo, maximo]
+
+}
+
+/* mensagem explicação basicamente adicionei duas varias uma infinity e outra infinity negativo porque se minimo e um valor infinity
+quando arr for < que minimo ele deixara de ser infinity e passará a ser o valor minimo mesma logica usei no let maximo no final o return 
+chama as duas variaveis com os valores menores e maiores
+*/
